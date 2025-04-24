9dfc57aafa4049427fd9619b79f1139d26b39dae
Your Name <you@example.com>
2025-04-24 19:56:01 +0600

    backup

--- flask-hello-world/hello.py
+++ flask-hello-world/hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     host_name = socket.gethostname()
-    return 'Hello from instance: {} \n'.format(host_name)    
+    return 'bye from instance: {} \n'.format(host_name)    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000,debug=True)
--- fullStackFlaskNotes/website/views.py
+++ fullStackFlaskNotes/website/views.py
@@ -1,31 +1,31 @@
 from flask import Blueprint, render_template, request, flash, jsonify
-from flask_login import login_required,current_user
+from flask_login import login_required, current_user
 from .models import Note
 from . import db
 import json
+import platform  # 👈 Added for OS name
 
+views = Blueprint('views', __name__)
 
-
-views = Blueprint('views',__name__)
-
-@views.route('/',methods=['GET','POST'])
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
         note = request.form.get('note')
-        if len(note)<1:
-            flash('Note is too short',category='error')
+        if len(note) < 1:
+            flash('Note is too short', category='error')
         else:
-            new_note = Note(data=note,user_id=current_user.id)
+            new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash('Note added',category='success')
+            flash('Note added', category='success')
 
-    return render_template("home.html",user=current_user)
+    os_name = platform.system()  # 👈 Added line
+    return render_template("home.html", user=current_user, os_name=os_name)  # 👈 Passed to template
 
 @views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+def delete_note():
+    note = json.loads(request.data)  # this function expects a JSON from the INDEX.js file
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
@@ -34,4 +34,3 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
-

f54fdf892df61369f0d4171c2051f8c8bddbcf71
Your Name <you@example.com>
2025-04-28 20:28:56 +0600

    backup

--- mern/chat-app-react-nodejs(done)/server/index.js
+++ mern/chat-app-react-nodejs(done)/server/index.js
@@ -16,7 +16,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("DB Connetion Successfull");
+    console.log("DB Connetion Successfull from: "+process.env.C_NAME);
   })
   .catch((err) => {
     console.log(err.message);

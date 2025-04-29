43fb8001be03cf9ec286242f7eea53d92f4105d0
Hasan Ashab <samertask@gmail.com>
2025-04-29 11:47:34 +0600

    backup

--- pern/Postgresql-reactjs/frontend/vite.config.js
+++ pern/Postgresql-reactjs/frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.VITE_PORT) || 5173, // fallback to 5173 if not defined
+  },
 })

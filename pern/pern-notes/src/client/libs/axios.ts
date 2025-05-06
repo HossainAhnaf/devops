28a83e153e65f6ce1763f3dbd48ff70a5c49f0f3
Your Name <you@example.com>
2025-05-06 11:30:06 +0600

    backup

--- pern/pern-notes/src/client/libs/axios.ts
+++ pern/pern-notes/src/client/libs/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : 'http://localhost:5000/api';
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const axiosAuth = axios.create({
   baseURL,
--- pern/pern-notes/vite.config.ts
+++ pern/pern-notes/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
  server: {
   host: '0.0.0.0',
   port: 3000,
-  allowedHosts: ['*']
+  allowedHosts: ['ip172-18-0-72-d0cp64ol2o9000a5rne0-3000.direct.labs.play-with-docker.com']
 }
-    
+
 })
